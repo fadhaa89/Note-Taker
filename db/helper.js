f9b6e2834e83f285feb63f6faf2c897d50e73843
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-04-16 22:57:38 -0500

    Retrieves all the notes in db.json and store in allNotes array

--- db/helper.js
+++ db/helper.js
@@ -1,39 +1,61 @@
+const fs = require('fs');
+const path = require('path');
+const { all } = require('../routes/apiRoutes');
 let allNotes = [];
 
-// Gets all the notes
-app.get("/api/notes", function(req, res) {
-    let notes = allNotes || [];
-    res.json(notes);
-});
 
+class Helper {
+    readNotes() {
+        return fs.readFileSync('./db.json', "utf-8");
 
-
-// Retrieves all the notes in db.json and store in allNotes array
-function gatherNotes() {
-    var rawNotes = fs.readFileSync(path.join(__dirname, "db", "db.json"))
-    return JSON.parse(rawNotes)
-}
-// Add a new note to the allNotes array
-function addNote(newNote) {
-    allNotes.push(newNote);
-    updateNotes(allNotes);
-}
-function updateNotes(notes) {
-    fs.writeFile(path.join(__dirname, "db", "db.json"), JSON.stringify(notes), err => {
-        if (err) throw err;
-    });
-}
-function deleteNote(id) {
-    const noteToDelete = allNotes.find(note => note.id === id);
-    if (!noteToDelete) {
-        return;
     }
-    // Remove the note from the notes array
-    allNotes.splice(allNotes.indexOf(noteToDelete), 1);
-    // Update the db.json file
-    updateNotes(allNotes);
-    console.log('Note to delete:' + noteToDelete);
-    return noteToDelete;
+
+    writeNotes(note){
+        return fs.writeFileSync('./db.json', JSON.stringify (note));
+      
+
+
+    }
+    // Retrieves all the notes in db.json and store in allNotes array
+    gatherNotes() {
+
+        return this.readNotes().then ((notes)=> {
+            let allNotes;
+
+            try{
+                allNotes= [].concat(JSON.parse(notes))
+            }catch(err){
+                allNotes =[]
+            }
+            return allNotes;
+        })
+    }
+    // Add a new note to the allNotes array
+    //to do ..
+    //getback in object -Tittle+Text //
+
+    //add an id
+    addNote(newNote) {
+        allNotes.push(newNote);
+        updateNotes(allNotes);
+    }
+    updateNotes(notes) {
+        fs.writeFile(path.join(__dirname, "db", "db.json"), JSON.stringify(notes), err => {
+            if (err) throw err;
+        });
+    }
+    deleteNote(id) {
+        const noteToDelete = allNotes.find(note => note.id === id);
+        if (!noteToDelete) {
+            return;
+        }
+        // Remove the note from the notes array
+        allNotes.splice(allNotes.indexOf(noteToDelete), 1);
+        // Update the db.json file
+        updateNotes(allNotes);
+        console.log('Note to delete:' + noteToDelete);
+        return noteToDelete;
+    }
 }
 
-
+module.exports = new Helper();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,28 @@
-const router = require ('express').Router();
-const helper = require ("../db/helper")
+const router = require('express').Router();
+const helper = require("../db/helper");
+
+// Gets all the notes
+router.get("/notes", function (req, res) {
+    helper.gatherNotes()
+        .then((notes) => {
+            return res.json(notes);
+        })
+        .catch((err) => res.status(500).json(err))
+
+});
 
 // POST `/api/notes` - Should recieve a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
-router.post("/api/notes", function(req, res) {
-    var newNote = req.body;
-    newNote.id = Date.now();
-    addNote(newNote);
-    updateNotes(allNotes);
-    res.json(newNote);
-    console.log('New note appended: ' + JSON.stringify(newNote));
-    return newNote;
+router.post("/notes", function (req, res) {
+    helper.updateNotes(req.body)
+        .then((notes) => {
+            return res.json(notes);
+        })
+        .catch((err) => res.status(500).json(err))
+
 });
 
 // Delete a note 
-router.delete("/api/notes/:id", function(req, res) {
+router.delete("/notes/:id", function (req, res) {
     let noteId = req.params.id;
     res.json(deleteNote(noteId));
 });
